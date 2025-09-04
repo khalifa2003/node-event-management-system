3b8cbc813bd9798b462bb0e83daef0d83d24da38
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-09-04 18:20:43 +0300

    s

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const swaggerDocs = require("./swagger");
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -14,6 +15,7 @@ const globalError = require('./middlewares/errorMiddleware');
 const dbConnection = require('./config/database');
 // Routes
 const mountRoutes = require('./routes');
+const PORT = process.env.PORT || 8000;
 
 // Connect with db
 dbConnection();
@@ -21,13 +23,15 @@ dbConnection();
 // express app
 const app = express();
 
+// Swagger docs
+swaggerDocs(app);
+
 // Enable other domains to access your application
 app.use(cors());
 app.options('*', cors());
 
 // compress all responses
 app.use(compression());
-
 
 // Middlewares
 app.use(express.json({ limit: '20kb' }));
@@ -38,32 +42,11 @@ if (process.env.NODE_ENV === 'development') {
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-// Limit each IP to 100 requests per `window` (here, per 15 minutes)
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-  message:
-    'Too many accounts created from this IP, please try again after an hour',
-});
-
-// Apply the rate limiting middleware to all requests
-app.use('/api', limiter);
-
-// Middleware to protect against HTTP Parameter Pollution attacks
-app.use(
-  hpp({
-    whitelist: [
-      'price',
-      'sold',
-      'quantity',
-      'ratingsAverage',
-      'ratingsQuantity',
-    ],
-  })
-);
-
 // Mount Routes
 mountRoutes(app);
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "server work" });
+});
 
 app.all('*', (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
@@ -72,11 +55,10 @@ app.all('*', (req, res, next) => {
 // Global error handling middleware for express
 app.use(globalError);
 
-const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => {
-  console.log(`App running running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${process.env.BASE_URL}`);
+  console.log(`Docs available at ${process.env.BASE_URL}/api-docs`);
 });
-
 // Handle rejection outside express
 process.on('unhandledRejection', (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
